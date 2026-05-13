0d58e7a1e1981d9a7a45b59496673fffcfbc1a56
Usuario <jorgejimenezgarcia1419hola@gmail.com>
2026-05-13 18:08:31 +0200

    Correcto funcionamiento implementado

--- ElConcursillo/src/ConexionMongoDB.java
+++ ElConcursillo/src/ConexionMongoDB.java
@@ -1,3 +1,7 @@
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
@@ -11,8 +15,15 @@ public class ConexionMongoDB {
 
     public static MongoDatabase conectar() {
         if (client == null) {
-            String connectionString = "mongodb+srv://daniclosy:millonario2026@proyecto.wepnsln.mongodb.net/";
-            client = MongoClients.create(connectionString);
+        	String connectionString = "mongodb+srv://jorgejimenezgarcia1419hola_db_user:millonario2026@proyecto.wepnsln.mongodb.net/Concurso?retryWrites=true&w=majority&appName=PROYECTO";
+            ServerApi serverApi = ServerApi.builder()
+                    .version(ServerApiVersion.V1)
+                    .build();
+            MongoClientSettings settings = MongoClientSettings.builder()
+                    .applyConnectionString(new ConnectionString(connectionString))
+                    .serverApi(serverApi)
+                    .build();
+            client = MongoClients.create(settings);
             database = client.getDatabase("Concurso");
         }
         return database;
